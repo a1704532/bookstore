a62ae7dd69353a32cf7627bb0c4e6e57b92b1079
a1704532 <mikael.kotila@wippies.fi>
2019-09-08 17:58:58 +0300

    First commit

---
+++ src/main/java/bookstore/Bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package bookstore.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/index")
+	public String book() {
+		return "index";
+	}
+}
