9798a019f49d791202f2f5eebec16272b8417dfa
a1704532 <mikael.kotila@wippies.fi>
2019-09-15 22:42:06 +0300

    Lisää lisäyksiä

--- src/main/java/bookstore/Bookstore/web/BookController.java
+++ src/main/java/bookstore/Bookstore/web/BookController.java
@@ -1,10 +1,12 @@
 package bookstore.Bookstore.web;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import bookstore.Bookstore.domain.Book;
 import bookstore.Bookstore.domain.BookRepository;
@@ -20,7 +22,32 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 
 		return "booklist";
+	}
 
+	@RequestMapping(value = "/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+	
+	@RequestMapping(value = "/edit/{id}")
+
+	public String addStudent(@PathVariable("id") Long bookId, Model model){
+	model.addAttribute("book", repository.findById(bookId));
+
+	return "editbook";
 	}
 
 	@GetMapping("/index")
