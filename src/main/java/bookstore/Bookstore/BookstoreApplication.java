f4c57224dbdd51a59b466adae1d3f3e0523b03a7
a1704532 <mikael.kotila@wippies.fi>
2019-10-07 03:53:36 +0300

    Security

--- src/main/java/bookstore/Bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import bookstore.Bookstore.domain.Book;
 import bookstore.Bookstore.domain.BookRepository;
 import bookstore.Bookstore.domain.Category;
 import bookstore.Bookstore.domain.CategoryRepository;
+import bookstore.Bookstore.domain.User;
+import bookstore.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication implements CommandLineRunner {
@@ -18,36 +20,25 @@ public class BookstoreApplication implements CommandLineRunner {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository,
+			UserRepository urepository) {
 		return (args) -> {
 
 			crepository.save(new Category("Romaani"));
 			crepository.save(new Category("Elämänkerrat"));
-			
-			repository.save(new Book("Harry Potter ja viisasten kivi", "J.K. Rowling", 1998, "9789513111465", 13, crepository.findByName("Romaani").get(0)));
-			repository.save(new Book("Harry Potter ja Feeniksin kilta", "J.K. Rowling", 2003, "9788498383621", 14, crepository.findByName("Romaani").get(0)));
 
-			
-			
-			
-			
-			/*
-			 @Bean
-	public CommandLineRunner studentDemo(StudentRepository srepository, DepartmentRepository drepository) {
-		return (args) -> {
-			log.info("save a couple of students");
-			drepository.save(new Department("IT"));
-			drepository.save(new Department("Business"));
-			drepository.save(new Department("Law"));
-			
-			srepository.save(new Student("John", "Johnson", "john@john.com", drepository.findByName("IT").get(0)));
-			srepository.save(new Student("Katy", "Kateson", "kate@kate.com", drepository.findByName("Business").get(0)));	
-			
-			log.info("fetch all students");
-			for (Student student : srepository.findAll()) {
-				log.info(student.toString());
-			}
-			 */
+			repository.save(new Book("Harry Potter ja viisasten kivi", "J.K. Rowling", 1998, "9789513111465", 13,
+					crepository.findByName("Romaani").get(0)));
+			repository.save(new Book("Harry Potter ja Feeniksin kilta", "J.K. Rowling", 2003, "9788498383621", 14,
+					crepository.findByName("Romaani").get(0)));
+
+			User user1 = new User("user", "$2a$10$nuHc9JW7ceOOOcR/b9i/hu/5ZHteDAS13pDb.j.7EIIPCrpjRB1P2",
+					"user@gmail.com", "USER");
+			User user2 = new User("admin", "$2a$10$gmmjZHOL8X.0kCnpq/5sj.J0jLPo0SLoDAfFkwVClNmyCBGY6sWZ2",
+					"admin@wippies.fi", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+
 		};
 
 	}
@@ -57,5 +48,4 @@ public class BookstoreApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 
 	}
-
 }
--- src/main/java/bookstore/Bookstore/web/BookController.java
+++ src/main/java/bookstore/Bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package bookstore.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,25 +10,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import bookstore.Bookstore.domain.Book;
 import bookstore.Bookstore.domain.BookRepository;
 import bookstore.Bookstore.domain.CategoryRepository;
+import bookstore.Bookstore.domain.UserRepository;
 
 @Controller
 public class BookController {
 
 	@Autowired
 	private BookRepository repository;
-	
+
 	@Autowired
 	private CategoryRepository crepository;
+	
+	@Autowired
+	private UserRepository urepository;
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		
+		
+		return "login";
+	}
 
 	@GetMapping("/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 
 		return "booklist";
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
 	}
 
 	@RequestMapping(value = "/addbook")
@@ -40,19 +65,19 @@ public class BookController {
 		repository.save(book);
 		return "redirect:booklist";
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
-    	repository.deleteById(bookId);
-        return "redirect:../booklist";
-    }
-	
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+
 	@RequestMapping(value = "/edit/{id}")
 
-	public String addStudent(@PathVariable("id") Long bookId, Model model){
-	model.addAttribute("book", repository.findById(bookId));
+	public String addStudent(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
 
-	return "editbook";
+		return "editbook";
 	}
 
 	@GetMapping("/index")
